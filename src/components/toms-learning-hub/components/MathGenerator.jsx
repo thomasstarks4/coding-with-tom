fe6bbf87f1b1cd9e03b33cf9ef2006140807eb98
thomasstarks4 <thomas.a.starks4@gmail.com>
2025-07-14 16:55:10 -0400

    Update Math Generator
    - Added complexity logic to allow for more advanced math problems, with 6 levels implemented
    - Added state-controlled, conditionally-rendered description view of complexity levels
    -  Added logic to select maximum number in generateProblems function based on the complexity level selected
    - Added logic to adjust the operator selection in generateProblems based on the complexity level selected
    - Restyled the component using TailwindCSS and leveraging existing custom CSS

--- src/components/toms-learning-hub/components/MathGenerator.jsx
+++ src/components/toms-learning-hub/components/MathGenerator.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
 
 function MathGenerator() {
-  // Store as a string so user can type freely
-  const [numProblems, setNumProblems] = useState("1");
+  const [numProblems, setNumProblems] = useState("10");
   const [problems, setProblems] = useState([]);
-
+  const [complexityLevel, setComplexityLevel] = useState(1);
+  const [showComplexityDescriptions, setShowComplexityDescriptions] =
+    useState(false);
   const generateProblems = (e) => {
+    //Complexity Overview:
+    // 1: Default - Simple Addition and subtraction between 1 and 10. No Negatives.
+    // 2: Simple Addition and Subtraction between 1 and 20. No Negatives.
+    // 3: Simple Addition and Subtraction between 1 and 50. No Negatives.
+    // 4: Simple Addition and Subtraction between 1 and 10. Negatives allowed.
+    // 5: Simple Addition and Subtraction between 1 and 20. Negatives allowed.
+    // 6: Simple Addition and Subtraction between 1 and 50. Negatives allowed.
+
     e.preventDefault(); // Prevent the form from refreshing the page
+    const generatedProblems = []; // Fresh array of problems
+    const getMaxNumber = () => {
+      switch (parseInt(complexityLevel)) {
+        case 2:
+        case 5:
+          return 20;
+        case 3:
+        case 6:
+          return 50;
+        default:
+          return 10;
+      }
+    };
+    // Helper function to generate a random number between 1 and the maximum number
+    const getRandomNumber = () =>
+      Math.floor(Math.random() * getMaxNumber()) + 1;
 
-    const generatedProblems = [];
-
-    // Helper function to generate a random number between 1 and 10
-    const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
-
-    // Helper function to randomly select an operator
-    const getOperator = (num1, num2) =>
-      num1 < num2 ? "+" : Math.random() < 0.5 ? "+" : "-";
+    // Helper function to select an operator. Behaves differently based on complexity level.
+    const getOperator = (num1, num2) => {
+      let operator = "";
+      switch (parseInt(complexityLevel)) {
+        case 4:
+        case 5:
+        case 6: // Allow negatives
+          operator = Math.random() > 0.5 ? "+" : "-";
+          return operator;
+        default: // Don't allow negatives
+          if (num1 <= num2) operator = "+";
+          else operator = Math.random() < 0.5 ? "+" : "-";
+          return operator;
+      }
+    };
 
     // Create the problems
     for (let i = 0; i < parseInt(numProblems, 10); i++) {
@@ -42,34 +74,143 @@ function MathGenerator() {
   };
 
   return (
-    <div className="col bg-black">
-      <h1 className="p-1 text-2xl text-white">Math Problem Generator</h1>
+    <div className="col bg-[#090a37] mb-4 ">
+      {/* Header */}
+      <h1 className="p-1 font-bold text-3xl text-white">
+        Math Problem Generator
+      </h1>
       <div className="noprint">
+        {/* This div is not included in printing */}
         <form className="m-4" id="problem-form" onSubmit={generateProblems}>
-          <label htmlFor="numProblems">Number of Problems:</label>
-          <input
-            className="text-black"
-            type="number"
-            id="numProblems"
-            value={numProblems}
-            name="numProblems"
-            min="1"
-            required
-            onChange={onInputChange}
-          />
-          <button className="startProblems" type="submit">
-            Generate Problems
-          </button>
+          {/* Complexity */}
+          <div className="p-4 py-8 rounded-xl bg-slate-800 text-white text-center flex flex-col w-full">
+            <label className="font-semibold mb-2" htmlFor="complexityLevel">
+              How complex do you want the problems to be?
+              {/* See Complexity Overview in generateProblems function */}
+            </label>
+            <div class="w-full">
+              {/* Descriptions */}
+              {showComplexityDescriptions && (
+                <div
+                  id="complexityDescriptions"
+                  className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-900 text-gray-300 min-h-screen min-w-full flex justify-center items-center`}
+                >
+                  <div className="bg-slate-800 rounded-xl shadow-lg p-8 max-w-md w-full">
+                    <h2 className="text-xl font-semibold text-white mb-6 text-center">
+                      Complexity Levels Overview
+                    </h2>
+                    <ul className="space-y-4 text-sm">
+                      <li>
+                        <strong className="text-white">Level 1:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 10, ensuring no negative results.
+                      </li>
+                      <li>
+                        <strong className="text-white">Level 2:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 20, ensuring no negative results.
+                      </li>
+                      <li>
+                        <strong className="text-white">Level 3:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 50, ensuring no negative results.
+                      </li>
+                      <li>
+                        <strong className="text-white">Level 4:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 10, allowing for negative results.
+                      </li>
+                      <li>
+                        <strong className="text-white">Level 5:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 20, allowing for negative results.
+                      </li>
+                      <li>
+                        <strong className="text-white">Level 6:</strong> Simple
+                        addition and subtraction problems using numbers between
+                        1 and 50, allowing for negative results.
+                      </li>
+                    </ul>
+                    <button
+                      className="mt-8 bg-slate-700 hover:bg-slate-600 text-white font-medium py-2 px-4 rounded-lg transition-all duration-200 w-full"
+                      onClick={() => setShowComplexityDescriptions(false)}
+                    >
+                      Go Back
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+            {/* Select Complexity Level */}
+            <div className="w-full">
+              <select
+                className="text-black justify-self-center-center w-20 text-xs text-center p-1 m-2 font-semibold rounded"
+                name="complexityLevel"
+                id="complexityLevel"
+                onChange={(e) => setComplexityLevel(e.target.value)}
+              >
+                <option value="1">Level 1</option>
+                <option value="2">Level 2</option>
+                <option value="3">Level 3</option>
+                <option value="4">Level 4</option>
+                <option value="5">Level 5</option>
+                <option value="6">Level 6</option>
+              </select>
+            </div>
+            {/* Complexity Level */}
+            <div className="text-sm">
+              <b>Complexity Selected:</b>{" "}
+              <span className="font-extrabold text-green-400">
+                Level {complexityLevel}
+              </span>
+            </div>
+            <button
+              type="button"
+              className="mt-4 bg-slate-600 mx-auto rounded hover:font-semibold hover:p-2 p-1 text-xs text-nowrap  w-4/6 transition-all duration-100"
+              onClick={() => setShowComplexityDescriptions(true)}
+            >
+              Show Complexity Descriptions
+            </button>
+          </div>
+          {/* Number of problems */}
+          <div class="p-4 py-8 rounded-xl bg-slate-800 text-white text-center flex flex-col mt-4 w-full">
+            <label className="font-semibold mb-4" htmlFor="numProblems">
+              How many problems do you want?
+            </label>
+            <input
+              className="text-black rounded p-2 mb-2 text-center"
+              type="number"
+              id="numProblems"
+              value={numProblems}
+              name="numProblems"
+              min="1"
+              required
+              onChange={onInputChange}
+            />
+            {/* Action Buttons */}
+            <div className="flex space-x-2 flex-row">
+              <button className="startProblems w-full" type="submit">
+                Generate Problems
+              </button>
+              <button
+                className="printProblems w-full"
+                type="button"
+                onClick={() => {
+                  window.print();
+                  console.log("Printing math problems...");
+                }}
+              >
+                Print Test!
+              </button>
+            </div>
+          </div>
         </form>
       </div>
 
-      {/* 
-        The key piece: using "whiteSpace: 'pre'" to preserve
-        newlines and spacing from the problemText strings.
-      */}
+      {/* Problems */}
       <div
         id="problems"
-        className="problems-container font-extrabold text-lg"
+        className="problems-container font-extrabold text-lg pb-4"
         style={{ whiteSpace: "pre" }}
       >
         {problems.map((problem, index) => (
